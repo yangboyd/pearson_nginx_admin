577d1f0fc2e587d476275a502152bfa4dca0d045
Ratnapala <VRATNOS@PEROOT.COM>
2016-03-31 12:54:14 +0530

    duplication change has been done

--- routes/index.js
+++ routes/index.js
@@ -161,30 +161,31 @@ function getformarr(type,cnf){
 		t= Object.keys(eval('conf.nginx.'+type));
 
 	}		
-	console.log('dhuhudf');
-var l=eval('conf.nginx.http');
+	//console.log('dhuhudf');
+
+	var l=eval('conf.nginx.http');
 
 	var z=JSON.stringify(l);
 
 	var pssed=JSON.parse(z)
 
-		console.log(pssed);
+	//	console.log(pssed);
 
-	for(var y in pssed){
+	// for(var y in pssed){
 
-		if(y.constructor == Object){
-			console.log(y.constructor);
-		}else{
-			//console.log(y);
-			console.log(y);
-			console.log(pssed[y].constructor);
-		}
-	}
+	// 	if(y.constructor == Object){
+	// 		console.log(y.constructor);
+	// 	}else{
+	// 		//console.log(y);
+	// 		console.log(y);
+	// 		console.log(pssed[y].constructor);
+	// 	}
+	// }
 
-	t.shift();
-	t.shift();
-	t.shift();
-	t.shift();
+	// t.shift();
+	// t.shift();
+	// t.shift();
+	// t.shift();
 
 	var arr=[];
 
@@ -193,14 +194,14 @@ var l=eval('conf.nginx.http');
 
 	if(type ==''){
 
-		for(var i in t) {  
+		for(var i in pssed) {  
 
 			
 
 			if(eval('conf.nginx.'+t[i]+'._value') == ''){
 				
 			}else{
-				arr.push({name:t[i],url:'conf.nginx.'+t[i], value:eval('conf.nginx.'+t[i]+'._value')});
+				arr.push({name:pssed[i],url:'conf.nginx.'+pssed[i], value:eval('conf.nginx.'+pssed[i]+'._value')});
 			}
 
 			
@@ -212,17 +213,18 @@ var l=eval('conf.nginx.http');
 
 	}else{
 
-			for(var i in t) {                    
-			    if(typeof t[i] === Object) {
+			for(var i in pssed) {                    
+			    if(pssed[i].constructor == Array) {
 			    	
-			    	//console.log(t[i]);
-			    	console.log(t[i] +' == '+  eval('conf.nginx.http.'+ t[i] +'._value'));
+			    	
+			    	console.log(i +' == '+  eval('conf.nginx.http.'+ i +'._value'));
+			    	arr.push({name:i,url:'conf.nginx.http.'+i, value:eval('conf.nginx.http.'+i)});
 
 			    } else {
 			    // Do another thing
 
-			     if(eval('conf.nginx.http.'+t[i]+'._value') == ''){
-			  		var j=Object.keys(eval('conf.nginx.http.'+t[i]))
+			     if(eval('conf.nginx.http.'+i+'._value') == ''){
+			  		var j=Object.keys(eval('conf.nginx.http.'+i))
 						  j.shift();
 						  j.shift();
 						  j.shift();
@@ -231,13 +233,13 @@ var l=eval('conf.nginx.http');
 
 			    		for(var k in j){
 
-			    			arr.push({name:t[i] +' - ' + j[k],url:'conf.nginx.http.'+t[i]+'.'+j[k],value:eval('conf.nginx.http.'+t[i]+'.'+j[k]+'._value')});
+			    			arr.push({name:i +' - ' + j[k],url:'conf.nginx.http.'+i+'.'+j[k],value:eval('conf.nginx.http.'+i+'.'+j[k]+'._value')});
 
 			    		}
 
 			    }else{
 
-			    		arr.push({name:t[i],url:'conf.nginx.http.'+t[i], value:eval('conf.nginx.http.'+t[i]+'._value')});
+			    		arr.push({name:i,url:'conf.nginx.http.'+i, value:eval('conf.nginx.http.'+i+'._value')});
 
 
 
